16f5d15bc4f2a56eb0174c5f02a106262e48cfe3
ZHELUK <60100053+ZHELUK@users.noreply.github.com>
2020-01-21 03:58:49 +0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,9 +1,3 @@
 echo "deb trusted=yes] https://yadominjinta.github.io/files/ termux extras" » $PREFIX/etc/apt/sources.list
-sleep 3
-pkg install atilo
-sleep 10
-atilo install debian
-sleep 10
-startdebian
-
-# thx overlamer1
+sleep 5
+echo "All okey, ZHELUK best :D"
